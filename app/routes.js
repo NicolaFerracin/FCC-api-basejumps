2884658c30499ee7b996e862d7d592ebb45d7d73
ni-kun <nicola.ferracin@gmail.com>
2016-01-04 14:57:08 +0100

    return IP

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,8 @@
 module.exports = function(app) {
 
 	app.get('/*', function(req, res, next) {
-		// instantiate null object if the time is not a Date or a unix timestamp
-		res.json({"IP" : req.connection.remoteAddress});
+		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+		res.json({"ipaddress" : ip});
 	});
 
 };
