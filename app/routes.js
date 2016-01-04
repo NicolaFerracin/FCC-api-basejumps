c2fcd37d46defea00f0d8fda3b5dc166b1cbb136
ni-kun <nicola.ferracin@gmail.com>
2016-01-04 15:11:43 +0100

    the app can now recognize ip, language and address from the browser request

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,11 @@ module.exports = function(app) {
 
 	app.get('/*', function(req, res, next) {
 		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-		res.json({"ipaddress" : ip});
+		var softwareRx = /\((.*?)\)/;
+		var userAgent = req.headers['user-agent'];
+		var software = userAgent.match(softwareRx)[1];
+		var language = req.headers['accept-language'].split(",")[0];
+		res.json({"ipaddress" : ip, "language" : language, "software" : software});
 	});
 
 };
